fd8e26c8afeeff3ba72ec4b68bb16081b6ed586f
Jeanne Barthélemy <47858126+Gaerdil@users.noreply.github.com>
2022-04-30 23:36:27 +0200

    Add files via upload

--- dataLoadInDataset.py
+++ dataLoadInDataset.py
@@ -1,28 +1,46 @@
-import scipy.io
+# import scipy.io
 import numpy as np
 import pandas as pd
 # import scipy as sc
-# from scipy.signal import decimate # TODO see later if needed the decimation step!!
+# from scipy.signal import decimate # TODO see later if needed the decimation step!! ===> Median step OK
 import random
+import mat73
+
 np.random.seed(0)
 random.seed(0)
 
-mat = scipy.io.loadmat('tom_data/data.mat')['dat'][0]
 
+# mat = scipy.io.loadmat('tom_data/data.mat')['dat'][0]
+data_dict = mat73.loadmat('data/data_prepro.mat')['dat']#[1]
+# print(data_dict)
+# print(len(data_dict))
+# print(data_dict[0].keys())
+# print(data_dict[0]['trig'])
+# print(len(data_dict[0]['trig']))
+# print(data_dict[0]['dat'][0].shape)
+# print(data_dict[0]['dat'][1].shape)
+# print(data_dict[0]['dat'][11].shape)
+# print(len(data_dict[0]['dat']))
 # print(mat.shape)
+
+
 
 def loadDataset(test_dataset_size = 500):
     dataset = {'sample_id':[],'labels':[], 'X':[]}
-    timestamp = 20
+    verification_check = len( data_dict[0]['dat'][0]) # in theory 125 lines :
     for subject in range(5):
-        label, x = list(mat[subject][0][0])[1], list(mat[subject][0][0])[3][0]
-        for i in range(len(label)):
-            if len(x[i]) >= timestamp: # drop short erroneous samples!
-                dataset['labels'].append(label[i][0])
-                dataset['X'].append(x[i][:timestamp]) # TODO change when real timestamp found!
-            # print(label[i][0])
-            # print(x[i])
-            # print(x[i].shape)
+        labels, x = data_dict[0]['trig'], data_dict[0]['dat']
+        # print(labels, x[0])
+        # print(x[0])
+        for i in range(len(labels)):
+            if len(x[i]) >= verification_check: # drop short erroneous samples!
+                # dataset['labels'].append(labels[i][0])
+                dataset['labels'].append(labels[i])
+                # print(len(x), len(x[0]))
+                dataset['X'].append(x[i])
+                # print(label[i][0])
+                # print(x[i])
+                # print(x[i].shape)
 
     nb_true = 0
     for i in range(len(dataset['labels'])):
@@ -47,7 +65,7 @@ def loadDataset(test_dataset_size = 500):
     train_ids = [id for id in dataset["sample_id"] if id not in test_ids]
 
     #===== display info
-    input_dims = [timestamp,8] # TODO change !! (when timestamp available)
+    input_dims = [verification_check,8] # TODO change !! (when timestamp available)
     print("Total dataset size: ", len(dataset['sample_id']), "test dataset size: ", test_dataset_size, "input dimensions: ", input_dims)
     print('dataset keys: ', dataset.keys())
     print("total number of targets: ", len([i for i in dataset['labels'] if i== 1]) ,"Number of targets in the testing set: ", len([dataset['labels'][i] for i in test_ids if dataset['labels'][i] == 1]) )
