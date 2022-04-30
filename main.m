c3443092752b62ab80cec1c74138af3a69f0c2b1
Tom Jefferis <tomjefferis@gmail.com>
2022-04-30 13:34:12 +0100

    qol fixes

--- main.m
+++ main.m
@@ -10,20 +10,20 @@ dat = {dat1,dat2,dat3,dat4,dat5};
 findat = {};
 
 for indexs = 1:numel(dat)
-    gbp = dat{indexs}
+    gbp = dat{indexs};
     A = gbp.trig;
     A1 = gbp.y;
 
     b = [1, -1];
-    c = ismember(A, b)
+    c = ismember(A, b);
     % Extract the elements of a at those indexes.
-    indexes = find(c)
+    indexes = find(c);
 
     index  = [indexes; size(A, 1) + 1];
     n      = numel(index) - 1;
     Result = cell(1, n);
     for k = 1:n
-        Result{k} = A1(index(k):index(k+1)-1, :);  % [EDITED]
+        Result{k} = A1(index(k):index(k+1)-1, :); 
     end
 
     % adding segmented trials to data
